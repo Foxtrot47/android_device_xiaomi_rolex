d44865341df06c397dda6ea4787046bdc206042d
Foxtrot47 <jjneutron@outlook.com>
2021-02-01 14:21:19 +0530

    rolex: Fluid Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rolex.mk
+    $(LOCAL_DIR)/fluid_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rolex-user \
-    lineage_rolex-userdebug \
-    lineage_rolex-eng
+    fluid_rolex-user \
+    fluid_rolex-userdebug \
+    fluid_rolex-eng
